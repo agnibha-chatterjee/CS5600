2ccf043e10af1fd84e6020ec88d7f0e7601237fb
agnibha-chatterjee <agni.bhaaa@gmail.com>
2025-02-25 00:46:38 -0800

    clean part1

--- assignment5/multithread.c
+++ assignment5/multithread.c
@@ -23,10 +23,10 @@ int main(void)
     pthread_create(&th1, NULL, worker, "X");
     pthread_create(&th2, NULL, worker, "Y");
     sleep(1);
-    printf("====> Cancelling Thread Y with thread ID %lu!!\n", (unsigned long)th2);
+    printf("====> Cancelling Thread Y with thread ID %lu!!\n", (long)th2);
     pthread_cancel(th2);
     usleep(100000);
-    printf("====> Cancelling Thread X with thread ID %lu!!\n", (unsigned long)th1);
+    printf("====> Cancelling Thread X with thread ID %lu!!\n", (long)th1);
     pthread_cancel(th1);
     printf("exiting from main program\n");
     return 0;
